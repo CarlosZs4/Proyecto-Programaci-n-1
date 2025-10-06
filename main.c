e4ed79db21945ccd99c480fa2cb98d59c0e7d1a8
Carlos <carlosazs121@gmail.com>
2025-10-06 18:13:31 -0400

    ajustando el menu

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 /*librerias estandar de C*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 /*-------------------------*/
 /*librerias del proyecto*/
 #include "AsistenteIA.h"
 /*-------------------------*/
 int menu();
+int loginInterface();
 
 int main()
 {
@@ -16,132 +17,215 @@ int main()
   Lista baseDatos;
   usuario *nuevoUsuario = (usuario *)malloc(sizeof(usuario));
   int op;
+  int continuar;
   crearL(&baseDatos);
   cargarBaseConocimiento(&baseDatos, "BaseConocimiento.txt");
   asistente.mensaje = (Pila *)malloc(sizeof(Pila));
   asistente.respuestaIA = (Pila *)malloc(sizeof(Pila));
   asistente.pendientes = (Pila *)malloc(sizeof(Pila));
-  if (asistente.mensaje == NULL || asistente.respuestaIA == NULL || asistente.pendientes == NULL) {
-      fprintf(stderr, "Error: No se pudo asignar memoria para las pilas.\n");
- 
-      if (asistente.mensaje) free(asistente.mensaje);
-      if (asistente.respuestaIA) free(asistente.respuestaIA);
-      exit(1);
+  if (asistente.mensaje == NULL || asistente.respuestaIA == NULL || asistente.pendientes == NULL)
+  {
+    fprintf(stderr, "Error: No se pudo asignar memoria para las pilas.\n");
+
+    if (asistente.mensaje)
+      free(asistente.mensaje);
+    if (asistente.respuestaIA)
+      free(asistente.respuestaIA);
+    exit(1);
   }
   crearP(asistente.mensaje);
   crearP(asistente.respuestaIA);
   crearP(asistente.pendientes);
   do
   {
-    op = menu();
-    while(op < 1 || op >6){
-    
+    continuar = loginInterface();
+    while (continuar < 1 || continuar > 4)
+    {
+
       system("clear");
       printf("ingrese una opción válida!\n");
       sleep(1);
-      op = menu();
+      continuar = loginInterface();
     };
-    switch (op)
+    switch (continuar)
     {
     case 1:
-    registrarUsuario();
-    break;
+      registrarUsuario();
+      break;
     case 2:
-    nuevoUsuario = iniciarSesion();
-    if (nuevoUsuario->sesionActiva)
-    {
-      system("clear");
-      printf("Bienvenido/a %s, ya puedes usar el asistente!\n",nuevoUsuario->nombre);
-      sleep(1);
-    }else{
-      system("clear");
-      printf("Datos incorrectos\n");
-      sleep(1);
-    }
-    
-    break;
-    case 3:
-    if (nuevoUsuario->sesionActiva || nuevoUsuario == NULL)
-    {
-      int continuar_conversacion = 1;
-      char resp[10];
-      system("clear");
-      printf("--- Asistente IA Iniciado ---\n");
-      printf("Escribe 'salir' para terminar la conversación.\n\n");
-      
-      while (continuar_conversacion) {
-          continuar_conversacion = conversacion(&asistente, &baseDatos,nuevoUsuario);
-      }
-      printf("\n--- Conversación Finalizada ---\n");
-      printf("Desea Deshacer su ultima peticion?\nNo\nSi\n");
-      printf("Elige una opcion: ");
-      scanf("%10s",resp);
-      if(strcmp(resp, "si") == 0 || strcmp(resp, "Si") == 0){
-          eliminarUltimaPeticion(&asistente);
-      }
-      system("clear");
-      printf("¡Hasta pronto!\n");
-      guardarHistorial(&asistente,nuevoUsuario,"Conversaciones.txt");
-      sleep(1);
-    }else
-    {
-      system("clear");
-      printf("La sesión debe estar activa!\n");
-      sleep(1);
-    }
-    
-    break;
-    case 4:
-    if (nuevoUsuario->sesionActiva){
-        mostrarHistorial(nuevoUsuario->cedula,"Conversaciones.txt");
-    }
-    else{
-      system("clear");
-      printf("La sesión debe estar activa!\n");
-      sleep(1);
-    }
-    break;
-    case 5:
-    break;
-    break;
-    case 6:
-    //preferiblemente esta respuesta debe ser del asistente
-    liberarL(&baseDatos);
-    liberarP(asistente.mensaje);
-    liberarP(asistente.respuestaIA);
-    liberarP(asistente.pendientes);
-    system("clear");
-    printf("Saliendo.\n");
-    usleep(700000);
-    system("clear");
-    printf("Saliendo..\n");
-    usleep(700000);
-    system("clear");
-    printf("Saliendo...\n");
-    usleep(700000);
+      nuevoUsuario = iniciarSesion();
+      if (nuevoUsuario->sesionActiva)
+      {
+        system("clear");
+        printf("Bienvenido/a %s, ya puedes usar el asistente!\n", nuevoUsuario->nombre);
+        sleep(1);
+        do
+        {
+          op = menu();
+          while (op < 1 || op > 4)
+          {
 
-    break;
+            system("clear");
+            printf("ingrese una opción válida!\n");
+            sleep(1);
+            op = menu();
+          };
+          switch (op)
+          {
+          case 1:
+            if (nuevoUsuario->sesionActiva)
+            {
+              int continuar_conversacion = 1;
+              char resp[10];
+              system("clear");
+              printf("--- Asistente IA Iniciado ---\n");
+              printf("Escribe 'salir' para terminar la conversación.\n\n");
+
+              while (continuar_conversacion)
+              {
+                continuar_conversacion = conversacion(&asistente, &baseDatos, nuevoUsuario);
+              }
+              printf("\n--- Conversación Finalizada ---\n");
+              printf("Desea Deshacer su ultima peticion?\nNo\nSi\n");
+              printf("Elige una opcion: ");
+              scanf("%10s", resp);
+              if (strcmp(resp, "si") == 0 || strcmp(resp, "Si") == 0)
+              {
+                eliminarUltimaPeticion(&asistente);
+              }
+              system("clear");
+              printf("¡Hasta pronto!\n");
+              guardarHistorial(&asistente, nuevoUsuario, "Conversaciones.txt");
+              sleep(1);
+            }
+            else
+            {
+              system("clear");
+              printf("La sesión debe estar activa!\n");
+              sleep(1);
+            }
+            break;
+          case 2:
+            if (nuevoUsuario->sesionActiva)
+            {
+              mostrarHistorial(nuevoUsuario->cedula, "Conversaciones.txt");
+            }
+            else
+            {
+              system("clear");
+              printf("La sesión debe estar activa!\n");
+              sleep(1);
+            }
+            break;
+          case 3:
+            // TULIO DEBE IMPLEMENTAR LAS ESTADISTICAS AQUI, debe validar si  el nuevoUsuario->sesionActiva es true, obviamente jsjs
+            break;
+          case 4:
+            system("clear");
+
+            char *msg1 = "Cerrando sesión";
+            char *msg2 = "...";
+            for (int i = 0; msg1[i] != '\0'; i++)
+            {
+              printf("%c", msg1[i]);
+              fflush(stdout);
+              usleep(50000);
+            }
+            usleep(500000);
+            for (int i = 0; msg2[i] != '\0'; i++)
+            {
+              printf("%c", msg2[i]);
+              fflush(stdout);
+              usleep(500000);
+            }
+            printf("\n");
+            break;
+          default:
+            break;
+          }
+        } while (op != 4);
+      }
+      else
+      {
+        system("clear");
+        printf("Datos incorrectos\n");
+        sleep(1);
+      }
+      break;
+    case 3:
+    //implementar el administrador (carlos)
+      break;
+    case 4:
+      liberarL(&baseDatos);
+      liberarP(asistente.mensaje);
+      liberarP(asistente.respuestaIA);
+      liberarP(asistente.pendientes);
+      system("clear");
+
+      char *msg1 = "Saliendo";
+      char *msg2 = "...";
+      for (int i = 0; msg1[i] != '\0'; i++)
+      {
+        printf("%c", msg1[i]);
+        fflush(stdout);
+        usleep(50000);
+      }
+      usleep(500000);
+      for (int i = 0; msg2[i] != '\0'; i++)
+      {
+        printf("%c", msg2[i]);
+        fflush(stdout);
+        usleep(500000);
+      }
+      printf("\n");
+
+      break;
+
     default:
       break;
     }
-  } while (op != 6);
+  } while (continuar != 4);
+
   return 0;
 }
 int menu()
 {
   int opcion;
-  char *menu[] = {"1. Registrar usuario", "2. Iniciar sesión", "3. Realizar consulta",
-     "4. Ver historial de conversaciones", "5. Ver estadísticas", "6. Salir"};
-     printf("\t Menu\n");
-  for (int i = 0; i < 6; i++)
+  char *recuadro = "*--------------------------------------------------*\n";
+  char *menu[] = {"1. Realizar consulta", "2. Ver historial de conversaciones", "3. Ver estadísticas",
+                  "4. Cerrar sesión"};
+  printf("%s", recuadro);
+  printf("\t\t Menu de Asistente IA\n");
+  for (int i = 0; i < 4; i++)
   {
-    printf("%s\n", menu[i]);
+    printf("\t%s\n", menu[i]);
   }
+  printf("%s", recuadro);
   printf("Elige una opción: ");
   scanf("%d", &opcion);
   int c;
 
-    while ((c = getchar()) != '\n' && c != EOF);
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
   return opcion;
-  
+}
+int loginInterface()
+{
+  int opcion;
+  char *recuadro = "*--------------------------------------------------*\n";
+  char *menu[] = {"1. Registrar usuario", "2. Iniciar sesión", "3. Iniciar sesión como Administrador", "4. Salir"};
+  printf("%s", recuadro);
+  printf("\t\t Menu\n");
+  for (int i = 0; i < 4; i++)
+  {
+    printf("\t%s\n", menu[i]);
+  }
+  printf("%s", recuadro);
+  printf("Elige una opción: ");
+  scanf("%d", &opcion);
+  int c;
+
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
+  return opcion;
 }
